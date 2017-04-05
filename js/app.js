ea8bd43c0715bae3c61fde6f9754a1e5f36ce665
gaimsfitness <gaimsfitnesscompanyrocks@gmail.com>
2017-04-05 07:11:45 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 var app = angular.module('ClubReadyCheckIn',['ngRoute']); //Place App Name in Single Quotes
 
-app.config(function($routeProvider){
-    $routeProvider
-        // Route for root page
-        .when('/', {
-            templateUrl : 'views/home/home.html',
-            controller : 'HomeController'
-        })
+//app.config(function($routeProvider){
+//    $routeProvider
+//        // Route for root page
+//        .when('/', {
+//            templateUrl : 'views/home/home.html',
+//            controller : 'HomeController'
+//        })
         
         // Location for new route, e.g. '/about'
         
         // Route for anyother path is tried, re-direct to root
-        .otherwise({ redirectTo : '/'});
-});
+//        .otherwise({ redirectTo : '/'});
+//});
