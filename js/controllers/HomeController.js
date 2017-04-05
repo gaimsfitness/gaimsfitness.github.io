e0353ac540f1f1f7152b8c7175dcc888c1d7370a
gaimsfitness <gaimsfitnesscompanyrocks@gmail.com>
2017-04-05 07:14:33 -0700

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,6 +3,7 @@ app.controller('HomeController',['$scope',function($scope){
     $scope.APIPath = "http://www.clubready.com/api/resource"
     $scope.name = "Content Goes Here";
     $scope.AllowEntry = true;
+    console.log(APIKEY);
     $scope.getUser = function(){
     
       $http.get($scope.APIPath+"?ApiKey="+$scope.APIKEY).then(function(response){
