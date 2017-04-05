4e825632e368df4a97387d780ce2e5dfd1c32001
gaimsfitness <gaimsfitnesscompanyrocks@gmail.com>
2017-04-05 07:20:43 -0700

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,4 +1,4 @@
-app.controller('HomeController',['$scope',function($scope){
+app.controller('HomeController',['$scope','$http',function($scope,$http){
     $scope.APIKEY = "xxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxx";
     $scope.APIPath = "http://www.clubready.com/api/resource"
     $scope.name = "Content Goes Here";
